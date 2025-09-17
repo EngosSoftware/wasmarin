a253251f87baaa8cd6dfd45bb9facfd7b7bfeb92
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-09-17 09:39:27 +0200

    Updates.

--- tests/test_simple_wasmtime.rs
+++ tests/test_simple_wasmtime.rs
@@ -1,4 +1,4 @@
-use wasmtime::{Engine, Instance, Module, Store};
+use wasmtime::{Config, Engine, Instance, Module, Store};
 
 #[test]
 fn a() {
@@ -15,13 +15,81 @@ fn a() {
   "#;
   let wasm_binary = wat::parse_str(wat_str).unwrap();
   // compile module
-  let engine = Engine::default();
+  let mut config = Config::new();
+  config.consume_fuel(true);
+  let engine = Engine::new(&config).unwrap();
   let module = Module::from_binary(&engine, &wasm_binary).unwrap();
   // instantiate
   let mut store = Store::new(&engine, ());
+  store.set_fuel(10_000).unwrap();
   let instance = Instance::new(&mut store, &module, &[]).unwrap();
   // get the function handle
   let add_one = instance.get_typed_func::<i32, i32>(&mut store, "add_one").unwrap();
   let output = add_one.call(&mut store, 2).unwrap();
   assert_eq!(3, output);
+  println!("{}", store.get_fuel().unwrap());
+}
+
+#[test]
+fn b() {
+  let wat_str = r#"
+    (module
+      (memory (export "memory") 1)
+      (data "Hello, World!")
+      (func (export "init_memory")
+        i32.const 10   ;; destination offset in memory
+        i32.const 0    ;; source offset in data segment
+        i32.const 5    ;; length
+        memory.init 0
+        data.drop 0
+      )
+    )
+    "#;
+  let wasm_binary = wat::parse_str(wat_str).unwrap();
+  // compile module
+  let mut config = Config::new();
+  config.consume_fuel(true);
+  let engine = Engine::new(&config).unwrap();
+  let module = Module::from_binary(&engine, &wasm_binary).unwrap();
+  // instantiate
+  let mut store = Store::new(&engine, ());
+  store.set_fuel(10_000).unwrap();
+  let instance = Instance::new(&mut store, &module, &[]).unwrap();
+
+  // get the function handle
+  let init = instance.get_typed_func::<(), ()>(&mut store, "init_memory").unwrap();
+  init.call(&mut store, ()).unwrap();
+
+  println!("{}", store.get_fuel().unwrap());
+}
+
+#[test]
+fn c() {
+  let wat_str = r#"
+    (module
+      (memory 1)
+      (func (export "copy_memory")
+        i32.const 2     ;; destination offset in memory
+        i32.const 0     ;; source offset in memory
+        i32.const 40000 ;; length
+        memory.copy
+      )
+    )
+    "#;
+  let wasm_binary = wat::parse_str(wat_str).unwrap();
+  // compile module
+  let mut config = Config::new();
+  config.consume_fuel(true);
+  let engine = Engine::new(&config).unwrap();
+  let module = Module::from_binary(&engine, &wasm_binary).unwrap();
+  // instantiate
+  let mut store = Store::new(&engine, ());
+  store.set_fuel(10_000).unwrap();
+  let instance = Instance::new(&mut store, &module, &[]).unwrap();
+
+  // get the function handle
+  let copy_memory = instance.get_typed_func::<(), ()>(&mut store, "copy_memory").unwrap();
+  copy_memory.call(&mut store, ()).unwrap();
+
+  println!("{}", store.get_fuel().unwrap());
 }
