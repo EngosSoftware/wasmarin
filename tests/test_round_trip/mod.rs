af56865931a54fdb534f2884660d3cb092668e05
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-09-18 17:32:52 +0200

    Updates.

--- tests/test_round_trip/mod.rs
+++ tests/test_round_trip/mod.rs
@@ -1,17 +1,9 @@
-use wasmarin::{Encoder, Parser};
-
 #[test]
 fn _0001() {
   let data = std::fs::read("./tests/test_round_trip/burner.wasm").unwrap();
-  let mut parser = Parser::new();
+  let mut parser = wasmarin::Parser::new();
   let model = parser.parse_wasm_bytes(&data).unwrap();
-  let mut encoder = Encoder::default();
+  let mut encoder = wasmarin::Encoder::default();
   let wasm_bytes = encoder.encode(model);
-
-  println!("{:?}", wasmparser::validate(&wasm_bytes).err());
-
-  //assert!(wasmparser::validate(&wasm_bytes).is_ok());
-
-  // let wat = wasmprinter::print_bytes(&wasm_bytes).unwrap();
-  // println!("{}", wat);
+  assert!(wasmparser::validate(&wasm_bytes).is_ok());
 }
