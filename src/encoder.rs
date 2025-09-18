1e6c96e7fbd749c84d495b4b0d0af04071042c0d
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-09-18 16:37:55 +0200

    Updates.

--- src/encoder.rs
+++ src/encoder.rs
@@ -28,7 +28,7 @@ impl Encoder {
     // Prepare the WebAssembly module.
     let mut module = wasm_encoder::Module::new();
 
-    // Encode the type section.
+    // Encode the TYPE SECTION.
     let mut type_section = wasm_encoder::TypeSection::new();
     for rec_group in model.rec_groups {
       let sub_types: Vec<wasm_encoder::SubType> = rec_group.types().cloned().map(map_sub_type).collect();
@@ -41,19 +41,19 @@ impl Encoder {
       }
     }
 
-    // Encode the import section.
+    // Encode the IMPORT SECTION.
     let mut import_section = wasm_encoder::ImportSection::new();
     for import in model.imports {
       import_section.import(import.module, import.name, map_type_ref(import.ty));
     }
 
-    // Encode the function section.
+    // Encode the FUNCTION SECTION.
     let mut function_section = wasm_encoder::FunctionSection::new();
     for function_index in model.function_indexes {
       function_section.function(function_index);
     }
 
-    // Encode the table section.
+    // Encode the TABLE SECTION.
     let mut table_section = wasm_encoder::TableSection::new();
     for table in model.tables {
       match table.init {
@@ -66,13 +66,13 @@ impl Encoder {
       }
     }
 
-    // Encode the memory section.
+    // Encode the MEMORY SECTION.
     let mut memory_section = wasm_encoder::MemorySection::new();
     for memory_type in model.memory_types {
       memory_section.memory(map_memory_type(memory_type));
     }
 
-    // Encode the global section.
+    // Encode the GLOBAL SECTION.
     let mut global_section = wasm_encoder::GlobalSection::new();
     let mut global_index = 0;
     for global in model.globals {
@@ -87,7 +87,7 @@ impl Encoder {
       global_section.global(metering.remaining_points_type(), &metering.remaining_points_initial_value());
     }
 
-    // Encode the export section.
+    // Encode the EXPORT SECTION.
     let mut export_section = wasm_encoder::ExportSection::new();
     for export in model.exports {
       export_section.export(export.name, map_export_kind(export.kind), export.index);
@@ -97,7 +97,7 @@ impl Encoder {
     if let Some(metering) = &mut self.metering {
       export_section.export("wasmarin_metering_remaining_points", wasm_encoder::ExportKind::Global, metering.remaining_points);
     }
-    // Encode the code section.
+    // Encode the CODE SECTION.
     let mut code_section = wasm_encoder::CodeSection::new();
     for mut code_section_entry in model.code_section_entries {
       let locals: Vec<(u32, wasm_encoder::ValType)> = code_section_entry.locals.drain(..).map(|(index, val_type)| (index, map_val_type(val_type))).collect();
