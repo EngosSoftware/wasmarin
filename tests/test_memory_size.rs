c194c8c753176f2bb34b877fa523e74bf0ee7a5d
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-10-01 11:28:13 +0200

    Updates.

--- tests/test_memory_size.rs
+++ tests/test_memory_size.rs
@@ -1,10 +1,15 @@
+///
+/// Demonstrates the use of the `memory.size` instruction in WebAssembly.
+///
+/// This example defines a module with one memory of size 7 pages (7 x 64kB).
+///
 #[test]
 fn memory_size_should_work() {
   let wat_str = r#"
     (module
       (memory (export "mem") 7)
       (func (export "fun") (result i32)
-        memory.size  ;; Return the size of the memory.
+        memory.size  ;; Return the size of the memory;  push: 7  stack: 7
       )
     )
     "#;
@@ -15,8 +20,7 @@ fn memory_size_should_work() {
   let instance = wasmtime::Instance::new(&mut store, &module, &[]).unwrap();
   let memory = instance.get_memory(&mut store, "mem").unwrap();
   let fun = instance.get_typed_func::<(), i32>(&mut store, "fun").unwrap();
-
-  // The size of the memory is 7 pages.
   assert_eq!(7, fun.call(&mut store, ()).unwrap());
   assert_eq!(7, memory.size(&mut store));
+  assert_eq!(458752, memory.data_size(&mut store));
 }
