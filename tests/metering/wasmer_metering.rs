621a0c51f334d1cc0413714fd69082a00f8abd9b
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-05-28 14:23:06 +0200

    Updates.

--- tests/metering/wasmer_metering.rs
+++ tests/metering/wasmer_metering.rs
@@ -4,46 +4,51 @@ fn wasmer_metering_memory_copy() {
   let wat_str = r#"
     (module
       (memory 1)
-      (global (export "oil") (mut i64) i64.const 0)  ;; Single additional global variable for tracking oil usage
+      (global (export "wasmer_metering_remaining_points") (mut i64) i64.const 0)  ;; Remaining points
+      (global (export "wasmer_metering_points_exhausted") (mut i32) i32.const 0)  ;; Points exhausted flag: 0-not exhausted, 1-exhausted
+      (global (export "wasmer_metering_bytes_length") (mut i32) i32.const 0)      ;; Cache for length of bulk-memory operation
+      (global (export "wasmer_metering_total_cost") (mut i64) i64.const 0)        ;; Cache for total cost of bulk-memory operation
       (func (export "fun")
-        (local i32 i64)    ;; Two additional local variables
+        i32.const 2          ;; Destination offset in memory.
+        i32.const 0          ;; Source offset in memory.
+        i32.const 12         ;; Memory length in bytes to be copied.
 
-        i32.const 2        ;; Destination offset in memory.
-        i32.const 0        ;; Source offset in memory.
-        i32.const 12       ;; Memory length in bytes to be copied.
+        ;; Begin of the injected code
 
-        ;; Begin of the additional code.
-
-        local.tee 0        ;; Save the length in a local variable
-        local.get 0        ;; Push the length from local variable
-        i64.extend_i32_u   ;; Convert i32 value to i64 value
-        i64.const 31       ;; Push (bulk_memory_operation_unit - 1)
-        i64.add            ;; Add (length + (bulk_memory_operation_unit - 1))
-        i64.const 32       ;; Push bulk_memory_operation_unit
-        i64.div_u          ;; Divide ((length + (bulk_memory_operation_unit - 1)) / bulk_memory_operation_unit)
-        i64.const 13       ;; Push unit_cost
-        i64.mul            ;; Multiply (((length + (bulk_memory_operation_unit - 1)) / bulk_memory_operation_unit) * unit_cost)
-        i64.const 3        ;; Push accumulated_cost
-        i64.add            ;; Add ((((length + (bulk_memory_operation_unit - 1)) / bulk_memory_operation_unit) * unit_cost) + accumulated_cost)
-        local.set 1        ;; Pop the calculated total cost from stack and save in a local variable
-        global.get 0       ;; Get the current oil
-        local.get 1        ;; Push the total cost
-        i64.sub            ;; Subtract the total cost from oil
-        global.set 0       ;; Save remaining oil
-        global.get 0       ;; Push the current oil
-        i64.const 0        ;; Push 0
-        i64.lt_s           ;; Is oil < 0
-        if ;; label = @1
-          unreachable      ;; Break execution
+        global.set 2         ;; Pop length and save in global variable
+        global.get 2         ;; Push length back for calculation
+        i64.extend_i32_u     ;; Convert i32 value to i64 value
+        i64.const 31         ;; Push <unitSize - 1>
+        i64.add              ;; Add (<length> + <unitSize - 1>)
+        i64.const 32         ;; Push <unitSize>
+        i64.div_u            ;; Divide ((<length> + <unitSize - 1>) / <unitSize>)
+        i64.const 13         ;; Push <unitCost>
+        i64.mul              ;; Multiply (((<length> + <unitSize - 1>) / <unitSize>) * <unitCost>)
+        i64.const 3          ;; Push <accumulatedCost>
+        i64.add              ;; Add ((((<length> + <unitSize - 1>) / <unitSize>) * <unitCost>) + <accumulatedCost>) = totalCost
+        global.set 3         ;; Pop <totalCost> and save in global variable
+        global.get 0         ;; Push <remainingPoints> for calculations
+        global.get 3         ;; Push <totalCost> for calculations
+        i64.lt_u             ;; <remainingPoints> < <totalCost>
+        if
+          i32.const 1        ;; Prepare exhausted flag
+          global.set 1       ;; Set exhausted global variable
+          unreachable        ;; Break execution
         end
+        global.get 0         ;; Push <remainingPoints> for calculations
+        global.get 3         ;; Push <totalCost> for calculations
+        i64.sub              ;; Subtract (<remainingPoints> - <totalCost>)
+        global.set 0         ;; Save <remainingPoints> in global variable
+        global.get 2         ;; Push <length> back for bulk-memory calculations
 
-        ;; End of the additional code.
+        ;; End of injected code
 
-        memory.copy        ;; Perform the copy memory if enough oil.
+        memory.copy          ;; Perform the copy memory if there are enough points
       )
       (export "mem" (memory 0))
     )
   "#;
+
   let wasm_bytes = wat::parse_str(wat_str).unwrap();
   let engine = wasmtime::Engine::default();
   let module = wasmtime::Module::from_binary(&engine, &wasm_bytes).unwrap();
