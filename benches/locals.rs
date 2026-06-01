10bd3cc2f55dde24254e4949ad31348d70864881
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-06-01 10:06:35 +0200

    Updates.

--- benches/locals.rs
+++ benches/locals.rs
@@ -59,8 +59,7 @@ fn _0004(b: &mut Bencher) {
 
 #[bench]
 fn _0005(b: &mut Bencher) {
-  const WAT: &str = include_str!("../tests/contracts/big_locals.wat");
-  let wasm_bytes = wat::parse_str(WAT).unwrap();
+  let wasm_bytes = wat::parse_str(create_wat(50000)).unwrap();
   let compiler = wasmer::sys::Singlepass::default();
   let mut store = wasmer::Store::new(compiler);
   let module = wasmer::Module::from_binary(&store, &wasm_bytes).unwrap();
@@ -72,8 +71,7 @@ fn _0005(b: &mut Bencher) {
 
 #[bench]
 fn _0006(b: &mut Bencher) {
-  const WAT: &str = include_str!("../tests/contracts/big_locals.wat");
-  let wasm_bytes = wat::parse_str(WAT).unwrap();
+  let wasm_bytes = wat::parse_str(create_wat(50000)).unwrap();
   let compiler = wasmer::sys::Singlepass::default();
   let mut store = wasmer::Store::new(compiler);
   let module = wasmer::Module::from_binary(&store, &wasm_bytes).unwrap();
@@ -81,4 +79,27 @@ fn _0006(b: &mut Bencher) {
   let fun = instance.exports.get_typed_function::<(), i32>(&store, "wrapper").unwrap();
   assert_eq!(10, fun.call(&mut store).unwrap());
   b.iter(|| _ = fun.call(&mut store).unwrap());
+}
+
+const TEMPLATE: &str = r#"(module
+  (memory 1)
+  (func $fun (export "fun") (result i32)
+    (local;;LOCAL;;)
+    i32.const 10
+  )
+  (func (export "wrapper") (result i32)
+    call $fun
+  )
+  (export "mem" (memory 0))
+)"#;
+
+fn create_wat(mut n: usize) -> String {
+  n = n.max(1);
+  let locals = " i32".repeat(n);
+  TEMPLATE.replace(";;LOCAL;;", &locals)
+}
+
+#[test]
+fn creating_wat_should_work() {
+  assert_eq!("(module\n  (memory 1)\n  (func $fun (export \"fun\") (result i32);\n    (local i32)\n    i32.const 10\n  )\n  (func (export \"wrapper\") (result i32)\n    call $fun\n  )\n  (export \"mem\" (memory 0))\n)", create_wat(1));
 }
