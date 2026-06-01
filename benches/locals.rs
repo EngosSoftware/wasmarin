5aa2a69afdfdb8bdfe033278204a2f970437645f
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-06-01 09:24:00 +0200

    Updates.

--- benches/locals.rs
+++ benches/locals.rs
@@ -56,3 +56,29 @@ fn _0004(b: &mut Bencher) {
   assert_eq!(10, fun.call(&mut store, ()).unwrap());
   b.iter(|| _ = fun.call(&mut store, ()).unwrap());
 }
+
+#[bench]
+fn _0005(b: &mut Bencher) {
+  const WAT: &str = include_str!("../tests/contracts/big_locals.wat");
+  let wasm_bytes = wat::parse_str(WAT).unwrap();
+  let compiler = wasmer::sys::Singlepass::default();
+  let mut store = wasmer::Store::new(compiler);
+  let module = wasmer::Module::from_binary(&store, &wasm_bytes).unwrap();
+  let instance = wasmer::Instance::new(&mut store, &module, &wasmer::imports! {}).unwrap();
+  let fun = instance.exports.get_typed_function::<(), i32>(&store, "fun").unwrap();
+  assert_eq!(10, fun.call(&mut store).unwrap());
+  b.iter(|| _ = fun.call(&mut store).unwrap());
+}
+
+#[bench]
+fn _0006(b: &mut Bencher) {
+  const WAT: &str = include_str!("../tests/contracts/big_locals.wat");
+  let wasm_bytes = wat::parse_str(WAT).unwrap();
+  let compiler = wasmer::sys::Singlepass::default();
+  let mut store = wasmer::Store::new(compiler);
+  let module = wasmer::Module::from_binary(&store, &wasm_bytes).unwrap();
+  let instance = wasmer::Instance::new(&mut store, &module, &wasmer::imports! {}).unwrap();
+  let fun = instance.exports.get_typed_function::<(), i32>(&store, "wrapper").unwrap();
+  assert_eq!(10, fun.call(&mut store).unwrap());
+  b.iter(|| _ = fun.call(&mut store).unwrap());
+}
