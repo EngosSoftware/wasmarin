f58a75f48c91d98bb4240ece1ee5ea977d950b86
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-07-19 13:32:00 +0200

    Updates.

--- benches/memory_copy.rs
+++ benches/memory_copy.rs
@@ -1,15 +1,35 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use std::time::Duration;
 
-const MEM_SIZE: [usize; 11] = [
+/// Lengths used for benchmarking.
+const LENGTHS: [usize; 30] = [
   1,
+  2,
+  5,
+  10,
+  20,
+  50,
   100,
+  200,
+  500,
   1_000,
+  2_000,
+  5_000,
   10_000,
+  20_000,
+  50_000,
   100_000,
+  200_000,
+  500_000,
   1_000_000,
+  2_000_000,
+  5_000_000,
   10_000_000,
+  20_000_000,
+  50_000_000,
   100_000_000,
+  200_000_000,
+  500_000_000,
   1_000_000_000,
   2_000_000_000,
   4_000_000_000,
@@ -27,19 +47,6 @@ const TEMPLATE: &str = r#"
 )
 "#;
 
-fn _0001(c: &mut Criterion) {
-  let mut group = c.benchmark_group("memory-copy");
-  for mem_size in MEM_SIZE {
-    let wasm_bytes = wat::parse_str(wat_source(mem_size)).unwrap();
-    let compiler = wasmer::sys::Singlepass::default();
-    let mut store = wasmer::Store::new(compiler);
-    let module = wasmer::Module::from_binary(&store, &wasm_bytes).unwrap();
-    let instance = wasmer::Instance::new(&mut store, &module, &wasmer::imports! {}).unwrap();
-    let fun = instance.exports.get_typed_function::<(), ()>(&store, "fun").unwrap();
-    group.bench_with_input(format!("size = {mem_size}"), &mem_size, |b, &_mem_size| b.iter(|| fun.call(&mut store).unwrap()));
-  }
-}
-
 fn wat_source(length: usize) -> String {
   TEMPLATE.replace("<LENGTH>", &length.to_string())
 }
@@ -50,6 +57,30 @@ fn make_config() -> Criterion {
     .measurement_time(Duration::new(20, 0))
     .sample_size(20)
     .configure_from_args()
+}
+
+fn _0001(c: &mut Criterion) {
+  let mut group = c.benchmark_group("memory-copy");
+  for length in LENGTHS {
+    let wasm_bytes = wat::parse_str(wat_source(length)).unwrap();
+    let compiler = wasmer::sys::Singlepass::default();
+    let store = wasmer::Store::new(compiler);
+    let module = wasmer::Module::from_binary(&store, &wasm_bytes).unwrap();
+    group.bench_with_input(format!("length = {length}"), &length, |b, _| {
+      b.iter_batched(
+        || {
+          let mut store = wasmer::Store::new(wasmer::sys::Singlepass::default());
+          let instance = wasmer::Instance::new(&mut store, &module, &wasmer::imports! {}).unwrap();
+          let fun = instance.exports.get_typed_function::<(), ()>(&store, "fun").unwrap();
+          (store, fun)
+        },
+        |(mut store, fun)| {
+          fun.call(&mut store).unwrap();
+        },
+        criterion::BatchSize::LargeInput,
+      );
+    });
+  }
 }
 
 criterion_group!(name = memory_copy; config = make_config(); targets = _0001);
