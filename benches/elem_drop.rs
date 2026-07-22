972356feb878e64dd3fa6c9310cc4f065eadcc95
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-07-22 15:27:45 +0200

    Updates.

--- benches/elem_drop.rs
+++ benches/elem_drop.rs
@@ -9,16 +9,18 @@ const MEASUREMENT_TIME: u64 = 5;
 const SAMPLE_SIZE: usize = 20;
 
 /// Lengths used for benchmarking.
-const LENGTHS: [usize; 52] = [
-  1, 2, 5, 10, 20, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1_000, 2_000, 3_000, 4_000, 5_000, 6_000, 7_000, 8_000, 9_000, 10_000, 20_000, 30_000, 40_000, 50_000, 60_000,
-  70_000, 80_000, 90_000, 100_000, 200_000, 300_000, 400_000, 500_000, 600_000, 700_000, 800_000, 900_000, 1_000_000, 2_000_000, 3_000_000, 4_000_000, 5_000_000, 6_000_000,
-  7_000_000, 8_000_000, 9_000_000, 10_000_000,
-];
+// const LENGTHS: [usize; 52] = [
+//   1, 2, 5, 10, 20, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1_000, 2_000, 3_000, 4_000, 5_000, 6_000, 7_000, 8_000, 9_000, 10_000, 20_000, 30_000, 40_000, 50_000, 60_000,
+//   70_000, 80_000, 90_000, 100_000, 200_000, 300_000, 400_000, 500_000, 600_000, 700_000, 800_000, 900_000, 1_000_000, 2_000_000, 3_000_000, 4_000_000, 5_000_000, 6_000_000,
+//   7_000_000, 8_000_000, 9_000_000, 10_000_000,
+// ];
+const LENGTHS: [usize; 8] = [1, 10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000];
 
 const TEMPLATE: &str = r#"
 (module
   (elem func <ELEMENTS>)
   (func $f1)
+  (func (export "warm"))
   (func (export "fun")
     elem.drop 0  ;; Drop passive element segment 0
   )
@@ -60,17 +62,20 @@ fn _0001(c: &mut Criterion) {
     let store = wasmer::Store::new(compiler);
     let module = wasmer::Module::from_binary(&store, &wasm_bytes).unwrap();
     group.bench_with_input(format!("L = {length}"), &length, |b, _| {
-      b.iter_batched(
+      b.iter_batched_ref(
         || {
-          let mut store = wasmer::Store::new(wasmer::sys::Singlepass::default());
+          let compiler = wasmer::sys::Singlepass::default();
+          let mut store = wasmer::Store::new(compiler);
           let instance = wasmer::Instance::new(&mut store, &module, &wasmer::imports! {}).unwrap();
+          let warm = instance.exports.get_typed_function::<(), ()>(&store, "warm").unwrap();
+          warm.call(&mut store).unwrap();
           let fun = instance.exports.get_typed_function::<(), ()>(&store, "fun").unwrap();
           (store, fun)
         },
-        |(mut store, fun)| {
-          fun.call(&mut store).unwrap();
+        |(store, fun)| {
+          fun.call(store).unwrap();
         },
-        criterion::BatchSize::LargeInput,
+        criterion::BatchSize::SmallInput,
       );
     });
   }
