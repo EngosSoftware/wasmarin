9149f73c38b247af85879253a03510300070442f
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-08-09 19:58:40 +0200

    Updates.

--- processed/linux-2026-07-23-12-40/table-grow.R
+++ processed/linux-2026-07-23-12-40/table-grow.R
@@ -10,8 +10,11 @@ coefs <- coef(model)
 print(coefs)
 
 # Estimating function
+base <- 200000000
+size <- 16
+cost <- 57344
 fe <- function(x) {
-  200000000 + (((x + 16 - 1) / 16) * 57344)
+  base + (((x + size - 1) / size) * cost)
 }
 
 # Estimated values
@@ -27,3 +30,4 @@ legend("bottomright", legend = sprintf("y = %.0f + %.2f x", coefs[1], coefs[2]),
 png("table-grow-log.png")
 plot(x, y, log = "x", main = "table.grow log Linux")
 lines(xe, ye, col = "magenta")
+legend("topleft", legend = sprintf(" base = %i\n size = %i\n cost = %i", base, size, cost), bty = "n")
