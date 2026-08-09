6573327b06b7eea2a837b1ef7e472096a717efc7
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-08-09 19:54:20 +0200

    Updates.

--- processed/linux-2026-07-23-12-40/memory-init.R
+++ processed/linux-2026-07-23-12-40/memory-init.R
@@ -10,8 +10,11 @@ coefs <- coef(model)
 print(coefs)
 
 # Estimating function
+base <- 3100000
+size <- 64
+cost <- 32768
 fe <- function(x) {
-  3100000 + (((x + 64 - 1) / 64) * 32768)
+  base + (((x + size - 1) / size) * cost)
 }
 
 # Estimated values
@@ -27,3 +30,4 @@ legend("bottomright", legend = sprintf("y = %.0f + %.2f x", coefs[1], coefs[2]),
 png("memory-init-log.png")
 plot(x, y, log = "x", main = "memory.init log Linux")
 lines(xe, ye, col = "magenta")
+legend("topleft", legend = sprintf(" base = %i\n size = %i\n cost = %i", base, size, cost), bty = "n")
