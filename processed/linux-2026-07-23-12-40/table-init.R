303d45594f180816f5a4213d75b5eb23077a121c
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-08-09 13:28:22 +0200

    Updates.

---
+++ processed/linux-2026-07-23-12-40/table-init.R
@@ -0,0 +1,29 @@
+# Input data
+x <- c(1,2,5,10,20,50,100,200,500,1000,2000,5000,10000,20000,50000,100000,200000,500000,1000000,2000000,5000000,9999999)
+y <- c(109980,111030,116860,123170,135740,175100,251600,378330,769230,1414600,2715000,6589400,13064000,25967000,64378000,129120000,258340000,736940000,1542400000,3173700000,8080900000,16240000000)
+
+# Linear regression
+model <- lm(y ~ x)
+coefs <- coef(model)
+
+# Print regression coefficients
+print(coefs)
+
+# Estimating function
+fe <- function(x) {
+  4600000 + (((x + 132 - 1) / 132) * 16384)
+}
+
+# Estimated values
+xe <- seq(1, 40000000000, by = 10000)
+ye <- fe(xe)
+
+png("table-init-reg.png")
+plot(x, y, main = "table.init reg Linux")
+abline(model, col = "blue")
+lines(xe, ye, col = "magenta")
+legend("bottomright", legend = sprintf("y = %.0f + %.2f x", coefs[1], coefs[2]), bty = "n")
+
+png("table-init-log.png")
+plot(x, y, log = "x", main = "table.init log Linux")
+lines(xe, ye, col = "magenta")
