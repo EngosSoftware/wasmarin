894fc0a638576b747e17131a10aa381a9ab55d86
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-08-09 13:20:55 +0200

    Updates.

---
+++ processed/linux-2026-07-23-12-40/table-copy.R
@@ -0,0 +1,29 @@
+# Input data
+x <- c(1,2,5,10,20,50,100,200,500,1000,2000,5000,10000,20000,50000,100000,200000,500000,1000000,2000000,5000000,9999999)
+y <- c(99013,99764,102730,106230,120070,117080,129200,156840,247650,421560,894890,2357600,5056900,10817000,27778000,57067000,126240000,330740000,946480000,2074000000,5485900000,11079000000)
+
+# Linear regression
+model <- lm(y ~ x)
+coefs <- coef(model)
+
+# Print regression coefficients
+print(coefs)
+
+# Estimating function
+fe <- function(x) {
+  4600000 + (((x + 132 - 1) / 132) * 16384)
+}
+
+# Estimated values
+xe <- seq(1, 40000000000, by = 10000)
+ye <- fe(xe)
+
+png("table-copy-reg.png")
+plot(x, y, main = "table.copy reg Linux")
+abline(model, col = "blue")
+lines(xe, ye, col = "magenta")
+legend("bottomright", legend = sprintf("y = %.0f + %.2f x", coefs[1], coefs[2]), bty = "n")
+
+png("table-copy-log.png")
+plot(x, y, log = "x", main = "table.copy log Linux")
+lines(xe, ye, col = "magenta")
